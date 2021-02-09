af2fd573cea17a6850d87c490c49a8538b671c60
Yakov Rakhamimov <jacobra19@gmail.com>
2021-02-10 00:11:18 +0200

    useDates custom hook, fixed matchup devision

--- pages/index.js
+++ pages/index.js
@@ -2,41 +2,26 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 
 import { isSunday } from 'date-fns'
-import { Container } from "@material-ui/core";
+import { Container, Button } from "@material-ui/core";
 
 import { ExpandingPanel, ErrorMessage, Loader, WeeklyDivider } from "../components";
+import { useDates } from '../hooks/hooks'
 
 export default function Home() {
-    const [dates, setDates] = useState([])
-    const [error, setError] = useState(null)
-
-    useEffect(async () => {
-        try {
-            let res = await fetch('/api/dates')
-            let parsed = await res.json()
-            let result = parsed.map(date => ({ ...date, time: new Date(date.time) }))
-            setDates(result)
-        } catch (error) {
-            setError(error)
-
-        }
-        return () => {
-        }
-    }, [])
+    const [matchups, loading, error, loadMore] = useDates()
 
     if (error) return <ErrorMessage />
+    const renderMatchups = matchups => matchups.map(renderExpPan)
 
     const renderExpPan = (day, i) => {
-
         return (
             <div key={i}>
-                { isSunday(day.time) || i === 0 ? <WeeklyDivider text={day.matchup} /> : null}
+                { i === 0 ? <WeeklyDivider text={day.matchup} /> : null}
                 <ExpandingPanel
                     isRookieStatusValid={day.isRookieStatusValid}
                     date={day.time}
                     rosters={day.rosters}
                 />
-
             </div>
         );
     };
@@ -48,7 +33,8 @@ export default function Home() {
                 <link rel='icon' href='/favicon.ico' />
             </Head>
             <Container maxWidth={"sm"} style={{ paddingTop: 24 }}>
-                {dates && dates.length ? dates.map(renderExpPan) : <Loader />}
+                {matchups && matchups.length ? matchups.map(renderMatchups) : <Loader />}
+                {/* <Button onClick={loadMore}>Load More</Button> */}
             </Container>
         </div>
     );
